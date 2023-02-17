f6d848f57bdbb7671306ace23f0d4e7d2fb19b3f
TessaViergever <112861180+TessaViergever@users.noreply.github.com>
2023-02-17 10:26:31 +0100

    add: wiki img + add: countrycodes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function getRandomInt(max) {
 }
 
 // TODO: alle landen aanvullen
-const possibleCountries = ['ARG', 'AUS', 'AUT', 'BEL', 'BGR', 'NLD', 'USA', 'CAN', 'THA', 'FRA']
+const possibleCountries = ['ARG', 'AUS', 'AUT', 'BEL', 'BGR', 'BRA', 'BWA', 'CAN', 'CHN', 'DEU', 'ECU', 'FIN', 'GTM', 'NLD', 'MDG', 'MAR', 'USA', 'CAN', 'THA', 'FRA', 'IND', 'IDN', 'MEX', 'PRT', 'ZAF', 'TUR', ]
 const maxFromPossibleCountries = possibleCountries.length - 1
 
 data.squad.members.forEach(function(member) {
@@ -52,109 +52,4 @@ app.listen(app.get('port'), function () {
 })
 
 
-//////////////////////////////////////////////////////////////////////////////////////////////
-
-
-// // Importeer express uit de node_modules map
-// import express from 'express'                         
-
-// const url = 'https://whois.fdnd.nl/api/v1/squad/'
-
-// // Maak een nieuwe express app
-// const app = express()
-
-// // Stel in hoe we express gebruiken (stel ejs in als template enige en geef de 'views' map door)
-// app.set('view engine', 'ejs')
-// app.set('views', './views')
-// app.use(express.static('public'))
-
-// // Gebruik de map 'public' voor statische resources
-// app.use(express.static('public'))
-
-// // Maak een route voor de index
-// app.get('/', (request, response) => {
-//   console.log(request.query.squad)
-
-//   let slug = request.query.squad || 'squad-a-2022'
-//   let orderBy = request.query.orderBy || 'name'
-//   let squadUrl = url + slug + '?orderBy=' + orderBy + '&direction=ASC'
-
-//   fetchJson(squadUrl).then((data) => {
-//     response.render('index', data)
-//   })
-// })
-
-// // app.get('/members', (request, response) => {
-// //   response.send('Joepie!!')
-// // })
-
-// // Stel het poortnummer in en start express
-// app.set('port', process.env.PORT || 8000)
-// app.listen(app.get('port'), function () {
-//   console.log(`Application started on http://localhost:${app.get('port')}`)
-// })
-
-// /**
-//  * Wraps the fetch api and returns the response body parsed through json
-//  * @param {*} url the api endpoint to address
-//  * @returns the json response from the api endpoint
-//  */
-// async function fetchJson(url) {
-//   return await fetch(url)
-//     .then((response) => response.json())
-//     .catch((error) => error)
-// }
-
-//////////////////////////////////////////////////////////////////////////////////////////////
-
-
-// Datamap inladen 
-
-// var map = new Datamap(
-//   {
-//       element: document.getElementById('container'),
-//       fills: {
-//           purple: 'purple',
-//           defaultFill: 'hotpink' // Any hex, color name or rgb/rgba value
-//       },
-//       data: {
-//           IRL: {
-//               fillKey: 'purple', // Any hex, color name or rgb/rgba value
-//               classmatesVisited: [
-//                   'Rosa',
-//                   'Michelle',
-//                   'Stefan'
-//               ]
-//           },
-//           USA: {
-//               fillKey: 'purple',
-//               classmatesVisited: [
-//                   'Tessa', // Any name (later: change to student id uit WHOIS API)
-//                   'Cyd',
-
-//               ]
-//           }
-//       },
-//       geographyConfig: {
-//           popupTemplate: function (geo, data) {
-//               let classmateNames = '';
-//               data.classmatesVisited.forEach((classmate, index) => {
-//                   if (index == data.classmatesVisited.length -1) {
-//                       classmateNames += classmate
-//                   } else {
-//                       classmateNames += classmate + ", "
-//                   }
-//               })
-
-//               return `
-//           <div class="hoverinfo">
-//               <strong>
-//                   ${geo.properties.name}
-//               </strong>
-//               <p>Zoveel klasgenoten zijn hier geweest: ${data.classmatesVisited.length}</p>
-//               <p>Dit zijn hun namen: ${classmateNames}</p>
-//           </div>`
-//           }
-//       }
-//   });
 
